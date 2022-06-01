431f032cc313fb7cca10a076d097e7adb02c79a3
Aleksandr Golamonov <alex-89fish@tut.by>
2022-06-01 15:44:56 +0300

    feat: copy js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,24 @@
+import fs from 'fs';
+const path = './files';
+const path_copy = './files_copy';
 export const copy = async () => {
-    // Write your code here 
+  let copy = [];
+   fs.readdir(path, function(err, items) {
+    if(err) {
+      console.log('FS operation failed')
+    } else {
+      if (!fs.existsSync(path_copy)){
+        fs.mkdirSync(path_copy);
+      } else {
+        console.log('FS operation failed')
+        return
+      }
+      for (let i=0; i<items.length; i++) {
+          copy = [...copy, items[i]]
+          fs.copyFile( `${path}/${items[i]}`, `${path_copy}/${items[i]}`, function(err) {} )
+      }
+    }
+});
 };
+
+copy()
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,28 +1,10 @@
 import fs from 'fs'
 
 export const create = async () => {
-   fs.promises.access('./files/fresh.txt', fs.constants.F_OK).then(() => console.log('Exist'))
+   fs.promises.access('./files/fresh.txt', fs.constants.F_OK).then(() => console.log('FS operation failed'))
   .catch(() => fs.writeFile('./files/fresh.txt', 'I am fresh and young', function (err) {
              if (err) throw err;
          console.log('Error');
-       }) );
-
-
-      fs.stat('foo.txt', function(err) {
-        if(err == null) {
-            console.log('File exists');
-        } else if(err.code === 'ENOENT') {
-          console.log('File exists');
-          fs.writeFile('log.txt', 'Some log\n', function (err) {
-            if (err) throw err;
-        console.log('Error');
-      });
-        } 
-    })
-       }
-
-
-    
-
-
+       }));
+      }
 create()
