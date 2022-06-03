4e730e33d88213d924dbbef98d5cace4ffe9f166
Aleksandr Golamonov <alex-89fish@tut.by>
2022-06-03 20:16:07 +0300

    feat: zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,14 @@
+import  zlib from 'zlib';
+import fs from 'fs';
+
 export const compress = async () => {
-    // Write your code here 
+  const gzip = zlib.createGzip();
+  const  read = fs.createReadStream('./files/fileToCompress.txt');
+  const  write = fs.createWriteStream('./files/archive.gz');
+  read.pipe(gzip).pipe(write).on('finish', () => {
+    fs.unlink('./files/fileToCompress.txt', err => {})
+  });
+
 };
+
+compress()
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,15 @@
+import  zlib from 'zlib';
+import fs from 'fs';
+
+
 export const decompress = async () => {
-    // Write your code here 
+  const gUnZip = zlib.createUnzip();
+  const  read = fs.createReadStream('./files/archive.gz');
+  const  write = fs.createWriteStream('./files/fileToCompress.txt');
+  read.pipe(gUnZip).pipe(write).on('finish', () => {
+      fs.unlink('./files/archive.gz', err => {})
+  });
+  
 };
+
+decompress()
